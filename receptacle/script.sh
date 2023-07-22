34d9f30782f43a01bb666a702f86665f1703cdef
Rey110 <140187346+Rey110@users.noreply.github.com>
2023-07-23 00:41:15 +0700

    Delete receptacle directory

--- receptacle/script.sh
+++
@@ -1,7 +0,0 @@
-echo ""
-    for app in $(cmd package list com.mobile.legends -3 | cut -f 2 -d ":"); do
-    cmd package compile -m speed -f com.mobile.legends
-    done
-    sleep 3
-    clear
-echo ""
